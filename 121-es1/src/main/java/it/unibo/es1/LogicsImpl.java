158bf12cbba076babf06a900820ac842b2ba77bd
Eleonora Bianco - eleonora.bianco3@studio.unibo.it <eleonora.bianco3@studio.unibo.it>
2024-12-10 17:09:03 +0100

    fiinished exercise 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,53 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class LogicsImpl implements Logics {
+	private List<Integer> values;
 
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		values = new ArrayList<>(Collections.nCopies(size, 0));
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return values;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return values.stream()
+			.map(d -> d != values.size())
+			.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		if (this.enablings().get(elem)) {
+			values.set(elem, values.get(elem) + 1);
+		}
+		return values.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return values.stream()
+			.map(t -> t.toString())
+			.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return values.stream()
+			.allMatch(d -> d == values.get(0));
 	}
 }
